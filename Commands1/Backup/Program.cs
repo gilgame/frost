0fa17db87ba0ac38c3d1636f75f77f1879d09c27
Tim <tdfellows@gmail.com>
2020-03-25 05:30:06 -0700

    Fix folder name

---
+++ Commands1/Backup/Program.cs
@@ -0,0 +1,195 @@
+﻿/* -----------------------------------------------------------------------------
+ * Program.cs
+ *
+ * Copyright (c) 2020 TD Fellows, TD Frost. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subjectto the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ * -----------------------------------------------------------------------------
+ */
+
+using Frost.Lib;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace Frost.Commands.Backup
+{
+    /// <summary>
+    /// Represents a program that simplifies calls to 7z.exe with custom parameters.
+    /// </summary>
+    public class Program : ConsoleHelper
+    {
+        private static string _ExePath;
+        private static readonly Config _Config = new Config();
+
+#if DEBUG
+        private static string[] GetArgs() => new string[]
+        {
+            //@"-v",
+            //@"C:\Users\Tim\source\repos\frost",
+            @"-h",
+        };
+#endif
+
+        /// <summary>
+        /// The main entry point.
+        /// </summary>
+        /// <param name="args">The command line arguments.</param>
+        public static void Main(string[] args)
+        {
+            PrintHeader();
+
+#if DEBUG
+            args = GetArgs();
+#endif
+
+            Init(args);
+
+            if (_Config.ShowHelp)
+            {
+                QuitUsage(_Config);
+            }
+
+            var filename = _ExePath;
+            var command = BuildCommand();
+
+            Console.WriteLine($"Attempting to run: {filename} {command}");
+
+            var result = 1;
+            try
+            {
+                var process = Process.Start(filename, command);
+                process.WaitForExit();
+                result = process.ExitCode;
+            }
+            catch (Exception ex)
+            {
+                QuitError(ex);
+            }
+
+            Quit(result);
+        }
+
+        private static void Init(string[] args)
+        {
+            AppDomain.CurrentDomain.UnhandledException += OnUnhandledException;
+
+            _Config.Parse(args);
+
+            if (!_Config.ShowHelp && _Config.Arguments.Count < 1)
+            {
+                throw new InitException("not enough files were supplied");
+            }
+
+            if (String.IsNullOrEmpty(_Config.CustomPath))
+            {
+                _ExePath = GetExePath();
+                if (String.IsNullOrEmpty(_ExePath))
+                {
+                    throw new InitException("7-zip is not correctly installed on this system");
+                }
+                else
+                {
+                    _ExePath = Path.Combine(_ExePath, "7z.exe");
+                }
+            }
+            else
+            {
+                _ExePath = _Config.CustomPath;
+            }
+        }
+
+        private static void OnUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            HandleException(_Config, e.ExceptionObject as Exception);
+        }
+
+        private static string GetExePath()
+        {
+            var exe = String.Empty;
+            var pathVariable = Environment.GetEnvironmentVariable("Path");
+
+            var paths = pathVariable.Split(';');
+            foreach (var path in paths)
+            {
+                if (path.Contains("7-zip", StringComparison.OrdinalIgnoreCase))
+                {
+                    exe = path;
+                    break;
+                }
+            }
+            return exe;
+        }
+
+        private static string BuildCommand()
+        {
+            List<string> args = new List<string>
+            {
+                String.Format("{0} {1}", _Config.Command, GetOutput().AddQutoes())
+            };
+
+            foreach (var file in _Config.Arguments)
+            {
+                args.Add(file.AddQutoes());
+            }
+
+            if (!String.IsNullOrEmpty(_Config.CustomFlags))
+            {
+                args.Add(_Config.CustomFlags);
+
+                return String.Join("", args);
+            }
+
+            if (_Config.Recursive)
+            {
+                args.Add("-r");
+            }
+
+            if (_Config.VsFlags)
+            {
+                args.Add(Config.VS_FLAGS);
+            }
+
+            if (_Config.GitFlags)
+            {
+                args.Add(Config.GIT_FLAGS);
+            }
+
+            var result = String.Join(" ", args);
+
+            return result;
+        }
+
+        private static string GetOutput()
+        {
+            if (!String.IsNullOrEmpty(_Config.OutputName))
+                return _Config.OutputName;
+
+            var path = Path.GetDirectoryName(_Config.Arguments[0]);
+            var filename = Path.GetFileNameWithoutExtension(_Config.Arguments[0]);
+            var file = Path.Combine(path, filename).TrimEnd('\\');
+
+            if (_Config.Timestamp)
+                return String.Format("{0}-{1}.7z", file, DateTime.Now.ToString("yyyyMMddHHmmss"));
+            else
+                return String.Format("{0}.7z", file);
+        }
+    }
+}
---
+++ Commands1/DomainUser/Program.cs
@@ -0,0 +1,204 @@
+﻿/* -----------------------------------------------------------------------------
+ * Program.cs
+ * 
+ * Copyright (c) 2020 TD Fellows, TD Frost. All rights reserved.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subjectto the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ * -----------------------------------------------------------------------------
+ */
+
+using Frost.Lib;
+using Frost.Lib.Parsers;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Frost.Commands.DomainUser
+{
+    /// <summary>
+    /// A program that calls runas with prompts and shortcuts.
+    /// </summary>
+    public class Program : ConsoleHelper
+    {
+        private class Csv : ICsvSettings
+        {
+            public int Delimiter => 44;
+            public bool FirstRowHeader => false;
+            public bool SkipEmptyRows => true;
+            public bool TrimFields => true;
+        }
+
+        private const string P_SHORTCUTS = "RunasShortcuts.json";
+
+        private static Config _Config = new Config();
+
+#if DEBUG
+
+        private static string[] GetArgs()
+        {
+            return new string[]
+            {
+                "-h",
+            };
+        }
+
+#endif
+
+        /// <summary>
+        /// The main entry point.
+        /// </summary>
+        /// <param name="args">The command line arguments.</param>
+        public static void Main(string[] args)
+        {
+            PrintHeader();
+
+#if DEBUG
+            args = GetArgs();
+#endif
+
+            Init(args);
+
+            if (_Config.ShowHelp)
+            {
+                QuitUsage(_Config);
+            }
+
+            //create shortcuts file and exit
+            if (_Config.CreateShortcuts)
+            {
+                Console.WriteLine("Creating shortcuts file");
+                _Config.MakeShortcuts(P_SHORTCUTS);
+
+                Quit(0);
+            }
+
+            var command = BuildCommand();
+
+            Console.WriteLine($"Attempting to run: runas {command}");
+
+            var result = 1;
+            try
+            {
+                var process = Process.Start("runas", command);
+                process.WaitForExit();
+                result = process.ExitCode;
+            }
+            catch (Exception ex)
+            {
+                QuitError(ex);
+            }
+
+            Quit(result);
+        }
+
+        private static void Init(string[] args)
+        {
+            AppDomain.CurrentDomain.UnhandledException += OnUnhandledException;
+
+            _Config.Parse(args);
+        }
+
+        private static void OnUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            if (e.ExceptionObject is ShortcutException)
+            {
+                var shortcut = e.ExceptionObject as ShortcutException;
+
+                QuitUsage(_Config, shortcut.Message);
+            }
+
+            HandleException(_Config, e.ExceptionObject as Exception);
+        }
+
+        private static string BuildCommand()
+        {
+            var command = new StringBuilder();
+
+            if (_Config.NetOnly)
+            {
+                command.Append("/netonly ");
+            }
+
+            var user = String.IsNullOrEmpty(_Config.Username)
+                ? PromptUser("Enter a user name:")
+                : _Config.Username;
+
+            command.Append($"/user:{user} ");
+
+            var path = String.IsNullOrEmpty(_Config.CommandPath)
+                ? GetShortcut()
+                : _Config.CommandPath;
+
+            command.Append($"{path.AddQutoes()}");
+
+            if (!String.IsNullOrEmpty(_Config.CustomFlags))
+            {
+                command.Append($"{_Config.CustomFlags} ");
+            }
+
+            return command.ToString().Trim();
+        }
+
+        private static string GetShortcut()
+        {
+            var key = _Config.Arguments.Count < 1
+                ? PromptUser("Enter a shortcut:")
+                : _Config.Arguments[0];
+
+            _Config.LoadShortcuts(P_SHORTCUTS);
+
+            var pair = _Config.Shortcuts.FirstOrDefault(k => k.Key.ToLower() == key.ToLower());
+            if (pair.Value == null)
+            {
+                throw new ShortcutException("shortcut not found");
+            }
+
+            return pair.Value;
+        }
+
+        private static string PromptUser(string message)
+        {
+            Console.WriteLine(message);
+
+            var shortcut = new StringBuilder();
+
+            ConsoleKeyInfo info;
+            while (((info = Console.ReadKey()).Key) != ConsoleKey.Enter)
+            {
+                if (info.Key == ConsoleKey.Escape)
+                {
+                    Console.WriteLine();
+                    throw new InitException("user cancelled");
+                }
+
+                shortcut.Append(info.KeyChar);
+            }
+            if (String.IsNullOrEmpty(shortcut.ToString()))
+            {
+                return PromptUser(message);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            return shortcut.ToString();
+        }
+    }
+}
